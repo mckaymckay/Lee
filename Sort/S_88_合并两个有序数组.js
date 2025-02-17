06350706230c7cb8bb7152ce15e0ccccc7a3f999
姜晓萌 <jiangxiaomeng@58.com>
2025-02-17 14:43:02 +0800

    push

---
+++ Sort/S_88_合并两个有序数组.js
@@ -0,0 +1,46 @@
+/**
+ * 题目链接：
+ * https://leetcode.cn/problems/merge-sorted-array/description/?envType=problem-list-v2&envId=sorting
+ * 
+ * 题目：
+ * 难度：
+ * 标签：
+ * 
+ * 题目描述：
+ * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * 
+ * 示例 1：
+ * 
+ * 备注：
+ * 
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+const nums1 = [1, 2, 3, 0, 0, 0]
+const m = 3, nums2 = [2, 5, 6], n = 3
+
+/**
+ * 方法一： 
+ * 时间复杂度：
+ * 空间复杂度：
+ */
+function func1 (nums1, m, nums2, n) {
+
+};
+
+var res1 = func1(nums1, m, nums2, n)
+console.log('res1:', res1)
+
+
+// 复习：
+function test (nums1, m, nums2, n) {
+
+}
